75ca093d6df2d85afdf3eac1dcad21676ca1fab1
Cihat Solak <cihatsolak@hotmail.com>
2021-01-23 00:34:39 +0300

    added cart-detail and checkout component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'root',
-  template: '<shop></shop>',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShopModule } from './shop/shop.module';
+import { RouterModule } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { CartDetailComponent } from './shop/cart-detail/cart-detail.component';
+import { CheckoutComponent } from './shop/checkout/checkout.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ShopModule],
+  imports: [
+    BrowserModule,
+    ShopModule,
+    RouterModule.forRoot([
+      { path: 'shop', component: ShopComponent },
+      { path: 'cart', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: '/shop' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
